08031438ac0906eba98c56bfc174a18da8f038bf
Joni Taajamo <joni.taajamo@outlook.com>
2018-05-07 12:04:12 +0300

    centered more tabels

--- helloreact/src/GithubRepos.jsx
+++ helloreact/src/GithubRepos.jsx
@@ -1,24 +1,23 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./App.css";
 
 class GitHubRepositories extends Component {
   constructor(props) {
     super(props);
-    this.state = { listItems: [],
-    inputValue: '' };
+    this.state = {
+      listItems: [],
+      inputValue: ''
+    };
   }
-
 
   fetchJsonData = () => {
     const searhTerm = this.state.inputValue;
     const url = "https://api.github.com/search/repositories?q=" + searhTerm;
     fetch(url)
-    .then(response => response.json())
-    .then(responseData => {
-      this.setState({
-        listItems: responseData.items,
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({listItems: responseData.items});
       });
-    });
   }
 
   onSubmit = () => {
@@ -30,18 +29,28 @@ class GitHubRepositories extends Component {
   }
 
   render() {
-    const itemRows = this.state.listItems.map(repo => (
-      <tr key={repo.full_name}>
-        <td>{repo.full_name}</td>
-        <td><a href={repo.html_url}>{repo.html_url}</a></td>
-      </tr>
-    ));
+    const itemRows = this
+      .state
+      .listItems
+      .map(repo => (
+        <tr key={repo.full_name}>
+          <td>{repo.full_name}</td>
+          <td>
+            <a href={repo.html_url}>{repo.html_url}</a>
+          </td>
+        </tr>
+      ));
 
     return (
       <div className="box has-text-centered">
-        <div class="field has-addons">
-          <div class="control">
-            <input class="input" type="text" placeholder="Find a repository" value={this.state.inputValue} onChange={this.onInputChange}/>
+        <div class="field has-addons has-addons-centered">
+          <div class="control centered-table">
+            <input
+              class="input"
+              type="text"
+              placeholder="Find a repository"
+              value={this.state.inputValue}
+              onChange={this.onInputChange}/>
           </div>
           <div class="control">
             <a class="button is-info" onClick={this.onSubmit}>
@@ -49,15 +58,17 @@ class GitHubRepositories extends Component {
             </a>
           </div>
         </div>
-        <table className="table">
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th>URL</th>
-            </tr>
-            {itemRows}
-          </tbody>
-        </table>
+        <div className="centered-table">
+          <table className="table">
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>URL</th>
+              </tr>
+              {itemRows}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
